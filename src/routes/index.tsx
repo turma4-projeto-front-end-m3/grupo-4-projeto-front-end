12e3876dee228e9feda80c7a6a96181b0cf9e66d
EmersonAvilaDev <avilaadsemerson@gmail.com>
2023-01-02 16:39:29 -0300

    branch/developer

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
+
+export const RoutesMain = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+
+      <Route path="/registerPage" element={<RegisterPage />} />
+
+      <Route path="/homePage" element={<HomePage />} />
+
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
+  );
+};
---
+++ src/styles/Global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+`
