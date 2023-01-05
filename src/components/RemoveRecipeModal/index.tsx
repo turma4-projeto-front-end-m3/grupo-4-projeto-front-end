34bd9b94444b763db57d47f57a30cf4ba211dc16
unknown <joaovicente985@msn.com>
2023-01-05 16:55:07 -0300

    feat: remove and edit modal

--- src/components/RemoveRecipeModal/index.tsx
+++ src/components/RemoveRecipeModal/index.tsx
@@ -1,7 +1,27 @@
+import { StyledRecipeModalBackground } from "../RecipeModal/styles";
+import { StyledRemoveModal } from "./styles";
+import { MdDeleteSweep } from "react-icons/md";
+import { BsCheck2All } from "react-icons/bs";
+
 export const RemoveRecipeModal = () => {
   return (
-    <>
-      <h1>RemoveRecipeModal</h1>
-    </>
+    <StyledRecipeModalBackground>
+      <StyledRemoveModal>
+        <header>
+          <button>X</button>
+        </header>
+        <div>
+          <h2>Tem certeza que deseja remover sua receita?</h2>
+          <div className="removeMiniContButtons">
+            <button className="confirm">
+              <BsCheck2All size={30} />
+            </button>
+            <button className="deny">
+              <MdDeleteSweep size={30} />
+            </button>
+          </div>
+        </div>
+      </StyledRemoveModal>
+    </StyledRecipeModalBackground>
   );
 };
--- src/components/RemoveRecipeModal/styles.ts
+++ src/components/RemoveRecipeModal/styles.ts
@@ -1,1 +1,68 @@
 import styled from "styled-components";
+
+export const StyledRemoveModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--light-gray);
+  max-width: 30rem;
+  max-height: 50rem;
+  border-radius: 0.5rem;
+
+  header {
+    display: flex;
+    flex-direction: row-reverse;
+    padding: 0.8rem 1rem;
+    background-color: var(--secondary-color);
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+
+    button {
+      background-color: var(--secondary-color);
+      border: none;
+      color: #ffffff;
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    padding: 2rem 3rem;
+
+    .removeMiniContButtons {
+      display: flex;
+      flex-direction: row;
+      gap: 0.5rem;
+
+      .confirm {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.5rem;
+        background-color: var(--secondary-color);
+        border: none;
+        color: #ffffff;
+        border-radius: 0.5rem;
+      }
+
+      .deny {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.5rem;
+        background-color: var(--background-color);
+        border: none;
+        color: #ffffff;
+        border-radius: 0.5rem;
+      }
+
+      button:hover {
+        opacity: 80%;
+      }
+    }
+  }
+`;
