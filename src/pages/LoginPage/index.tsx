44e875bb8476f2e8f14e7e81b4ffb346eaa397ca
Avila <Avilaadsemerson@gmail.com>
2023-01-09 19:44:40 -0300

    add: add/UserContext

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,57 +1,83 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.svg"
+import Logo from "../../assets/logo.svg";
 import { ContainerStyle, FormLogin, MainStyle } from "./styles";
-import { AiOutlineArrowRight } from "react-icons/ai"
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { iLoginData, UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+import { LoginFormSchema } from "./LoginFormSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<iLoginData>({
+    resolver: yupResolver(LoginFormSchema),
+  });
+  const { loginUser } = useContext(UserContext);
+
+  const submitLogin: SubmitHandler<iLoginData> = async (data) => {
+    await loginUser(data);
+  };
+
   return (
     <MainStyle>
       <ContainerStyle>
-
-      <div className="container2">
-        <section className="sectionDir">
-          
-          <div className="letsCooking">
+        <div className="container2">
+          <section className="sectionDir">
+            <div className="letsCooking">
               <h2>Vamos cozinhar?</h2>
               <p>Receitas despertam memórias, compartilhe-as!</p>
             </div>
 
-          <div className="divWelcome">
-            <h2>Olá</h2>
-            <h3>Bem vindo!</h3>
-          </div>
+            <div className="divWelcome">
+              <h2>Olá</h2>
+              <h3>Bem vindo!</h3>
+            </div>
 
-          <div className="divLogo">
-            <img src={Logo} alt="Logo Marca" />
-            <h1>MyRecipes</h1>
-          </div>
+            <div className="divLogo">
+              <img src={Logo} alt="Logo Marca" />
+              <h1>MyRecipes</h1>
+            </div>
+          </section>
 
-        </section>
-
-        <section className="sectionFormLogin">
+          <section className="sectionFormLogin">
             <h2>Login</h2>
-          <FormLogin>
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder="Digite seu email..." />
+            <FormLogin onSubmit={handleSubmit(submitLogin)}>
+              <label htmlFor="">Email</label>
+              <input
+                type="email"
+                placeholder="Digite seu email..."
+                {...register("email")}
+              />
+              {errors.email && <p>{errors.email.message}</p>}
 
-            <label htmlFor="">Senha</label>
-            <input type="password" placeholder="Digite sua senha..." />
+              <label htmlFor="">Senha</label>
+              <input
+                type="password"
+                placeholder="Digite sua senha..."
+                {...register("password")}
+              />
+              {errors.password && <p>{errors.password.message}</p>}
 
-            <Link to="/registerPage" className="forgotPass">Esqueceu sua senha?</Link>
+              <Link to="/registerPage" className="forgotPass">
+                Esqueceu sua senha?
+              </Link>
 
-            <Link to="/homePage" className="btnLogin">Entrar <AiOutlineArrowRight/> </Link>
+              <button className="btnLogin">
+                Entrar <AiOutlineArrowRight />{" "}
+              </button>
+            </FormLogin>
 
-          </FormLogin>
-
-        <div className="divRegister">
-          <p>Não possui uma conta?</p>
-          <Link to="/registerPage">Cadastre-se!</Link>
+            <div className="divRegister">
+              <p>Não possui uma conta?</p>
+              <Link to="/registerPage">Cadastre-se!</Link>
+            </div>
+          </section>
         </div>
-      </section>
-
-      </div>
       </ContainerStyle>
     </MainStyle>
-  
   );
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({})
+export const api = axios.create({
+    baseURL: "https://fake-api-grupo-4-0.onrender.com/",
+    timeout: 3500,
+}) 
